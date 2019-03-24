f4b5e6891bc586545963eb727b48a69fdbd7b69b
Davipb <daviparca@gmail.com>
2019-03-24 10:47:15 -0300

    Add time tracking

--- MultiplicativePersistence/Main.c
+++ MultiplicativePersistence/Main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <time.h>
 
 // Reports that a result has been found
 static void ReportResult(size_t steps, LargeNumber* number)
@@ -43,6 +44,24 @@ static size_t ReadConfig(char* buffer)
     return result;
 }
 
+// Formats and prints the duration between two points in time to stdout
+static void PrintDiff(time_t start, time_t end)
+{
+    double seconds = difftime(end, start);
+    printf("%dd %dh %dmin %ds", (int)(seconds / 86400), (int)(seconds / 3600), (int)(seconds / 60) % 60, (int)seconds % 60);
+}
+
+static void PrintTimeStats(time_t programStart, time_t deltaStart, time_t now)
+{
+    printf("Delta time: ");
+    PrintDiff(deltaStart, now);
+    printf("\n");
+
+    printf("Total time: ");
+    PrintDiff(programStart, now);
+    printf("\n");
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 4)
@@ -60,15 +79,27 @@ int main(int argc, char** argv)
     
     printf("Starting at %zu digits\n", start);
     printf("Ending at %zu digits\n", end);
-    printf("With a minimum of %zu steps\n", threshold);
+    printf("With a minimum of %zu steps\n", threshold);    
     
-    LargeNumber* current = SmallestWithDigits(start);
-    bool reportDigits = true;
+    time_t programStart = time(NULL);
+    uintmax_t numbersFound = 0;
 
+    LargeNumber* current = SmallestWithDigits(start);
+    time_t digitsStart = programStart;
+    bool reportDigits = true;
+    
     while (NumberOfDigits(current) <= end)
     {
         if (reportDigits)
+        {
+            time_t now = time(NULL);
+
+            printf("\n");
             printf("Now at %zu digits\n", NumberOfDigits(current));
+            PrintTimeStats(programStart, digitsStart, now);
+
+            digitsStart = now;
+        }
 
         size_t steps = 0;
         LargeNumber* acc = CopyNumber(current);
@@ -84,12 +115,20 @@ int main(int argc, char** argv)
         FreeNumber(acc);
 
         if (steps >= threshold)
+        {
+            numbersFound++;
             ReportResult(steps, current);
+        }
 
         reportDigits = Increment(current);
     }
 
     FreeNumber(current);
 
-    printf("Finished.\n");
+    time_t now = time(NULL);
+
+    printf("\n");
+    printf("Finished\n");
+    printf("Found %"PRIuMAX" results\n", numbersFound);
+    PrintTimeStats(programStart, digitsStart, now);
 }
